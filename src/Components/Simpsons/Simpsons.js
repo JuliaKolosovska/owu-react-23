74d430b6d1a1201a8a9f1deaa3e372ff3d33f196
JuliaKolosovska <Ykolosovska@gmail.com>
2023-04-11 11:02:34 +0200

    add

--- src/Components/Simpsons/Simpsons.js
+++ src/Components/Simpsons/Simpsons.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
 const Simpsons = (props) => {
-    let {itemName}=props;
-    let {itemImg}=props;
-    let {itemAge}=props;
+    let {itemName, itemImg, itemAge}=props;
+
 
 
     return (
@@ -11,7 +10,7 @@ const Simpsons = (props) => {
 
             <h3>{itemName}</h3>
             <h4>Age: {itemAge}</h4>
-            <img width={100} src={itemImg}/>
+            <img width={100} src={itemImg} alt={itemName}/>
 
 
         </div>
