6e312746f3f2f6e894c7f2adbacff4a82f402410
JuliaKolosovska <Ykolosovska@gmail.com>
2023-04-26 12:21:11 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hello
+        <div className="menu">
+            <h3>
+                <Link to={'/'}>Home</Link>
+            </h3>
+            <h3>
+                <Link to={'/users'}>Todos</Link>
+            </h3>
+            <h3>
+                <Link to={'/comments '}>Albums </Link>
+            </h3>
+                   </div>
+        <div>
+
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/todos'} element={<Todos/>}/>
+                <Route path={'/albums'} element={<Albums/>}/>
+                <Route path={'/comments'} element={<Comments/>}>
+                    <Route path={':postId'} element={<PostOfComment/>}/>
+                </Route>
+            </Routes>
+
+
+        </div>
     </div>
   );
 }
